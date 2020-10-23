00a901ca7cc57053662c03f42bc4b69dd7b85526
Łukasz Kurowski <crackcomm@gmail.com>
2020-10-23 09:17:12 +0200

    Modularized and not namespaced

--- src/x25519.gen.js
+++ src/x25519.gen.js
@@ -3,9 +3,11 @@
 
 import {default as loadNotChecked} from 'wasm-x25519';
 
-import {generate_keypair as generate_keypairNotChecked} from 'wasm-x25519';
-
 import {diffie_hellman as diffie_hellmanNotChecked} from 'wasm-x25519';
+
+import {derive_public as derive_publicNotChecked} from 'wasm-x25519';
+
+import {generate_keypair as generate_keypairNotChecked} from 'wasm-x25519';
 
 import {base64_encode as base64_encodeNotChecked} from 'wasm-x25519';
 
@@ -17,17 +19,23 @@ export const loadTypeChecked = loadNotChecked;
 // Export 'load' early to allow circular import from the '.bs.js' file.
 export const load = loadTypeChecked;
 
-// In case of type error, check the type of 'generate_keypair' in 'x25519.re' and 'wasm-x25519'.
-export const generate_keypairTypeChecked = generate_keypairNotChecked;
-
-// Export 'generate_keypair' early to allow circular import from the '.bs.js' file.
-export const generate_keypair = generate_keypairTypeChecked;
-
 // In case of type error, check the type of 'diffie_hellman' in 'x25519.re' and 'wasm-x25519'.
 export const diffie_hellmanTypeChecked = diffie_hellmanNotChecked;
 
 // Export 'diffie_hellman' early to allow circular import from the '.bs.js' file.
 export const diffie_hellman = diffie_hellmanTypeChecked;
+
+// In case of type error, check the type of 'derive_public' in 'x25519.re' and 'wasm-x25519'.
+export const derive_publicTypeChecked = derive_publicNotChecked;
+
+// Export 'derive_public' early to allow circular import from the '.bs.js' file.
+export const derive_public = derive_publicTypeChecked;
+
+// In case of type error, check the type of 'generate_keypair' in 'x25519.re' and 'wasm-x25519'.
+export const generate_keypairTypeChecked = generate_keypairNotChecked;
+
+// Export 'generate_keypair' early to allow circular import from the '.bs.js' file.
+export const generate_keypair = generate_keypairTypeChecked;
 
 // In case of type error, check the type of 'base64_encode' in 'x25519.re' and 'wasm-x25519'.
 export const base64_encodeTypeChecked = base64_encodeNotChecked;
--- src/x25519.ml
+++ src/x25519.ml
@@ -1,33 +1,41 @@
 (** OCaml (BuckleScript) bindings to lights0123/wasm-x25519. *)
 
+(** Initialize WASM asynchronously. *)
+external load : string -> unit Js.Promise.t = "load"
+  [@@genType.import "wasm-x25519", "default"]
+
+external diffie_hellman : string -> string -> string = "diffie_hellman"
+  [@@genType.import "wasm-x25519"]
+
+external derive_public : string -> string -> string = "derive_public"
+  [@@genType.import "wasm-x25519"]
+
+(** Calculate a shared secret from one party's secret key and another party's public key. *)
+let diffie_hellman ~secret ~public = diffie_hellman secret public
+
 module KeyPair = struct
   type t = string * string
+
+  (** Generates a keypair returning secret and public keys. *)
+  external create : unit -> t = "generate_keypair"
+    [@@genType.import "wasm-x25519"]
 
   (** Returns public part of key pair. *)
   let public_of ((_, public) : t) = public
 
   (** Returns secret part of key pair. *)
   let secret_of ((secret, _) : t) = secret
+
+  (** Derives a shared secret from key pair and public key. *)
+  let derive pair public = diffie_hellman ~secret:(secret_of pair) ~public
 end
 
-(** Initialize WASM asynchronously. *)
-external load : string -> unit Js.Promise.t = "load"
-  [@@genType.import "wasm-x25519", "default"]
+module Base64 = struct
+  (** Encodes as standard base64. *)
+  external encode : Js.Typed_array.Uint8Array.t -> string = "base64_encode"
+    [@@genType.import "wasm-x25519"]
 
-(** Generates a keypair returning secret and public keys. *)
-external generate_keypair : unit -> KeyPair.t = "generate_keypair"
-  [@@genType.import "wasm-x25519"]
-
-external diffie_hellman : string -> string -> string = "diffie_hellman"
-  [@@genType.import "wasm-x25519"]
-
-(** Calculate a shared secret from one party's secret key and another party's public key. *)
-let diffie_hellman ~secret ~public = diffie_hellman secret public
-
-(** Encodes as standard base64. *)
-external base64_encode : Js.Typed_array.Uint8Array.t -> string = "base64_encode"
-  [@@genType.import "wasm-x25519"]
-
-(** Decodes a standard base64. *)
-external base64_decode : string -> Js.Typed_array.Uint8Array.t = "base64_decode"
-  [@@genType.import "wasm-x25519"]
+  (** Decodes a standard base64. *)
+  external decode : string -> Js.Typed_array.Uint8Array.t = "base64_decode"
+    [@@genType.import "wasm-x25519"]
+end
